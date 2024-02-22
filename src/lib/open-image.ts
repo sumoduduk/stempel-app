54fb9d11106f8df4e8326b1fd3e2a516c44cbc44
sumoduduk <sumoduduk13@gmail.com>
2024-02-23 00:09:44 +0700

    fix: load image into view

--- src/lib/open-image.ts
+++ src/lib/open-image.ts
@@ -1,34 +1,36 @@
-import { convertFileSrc } from '@tauri-apps/api/core';
-import { getImages, getWatermarkImage } from './fs-utils';
-import { Setter } from 'solid-js';
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { getImages, getWatermarkImage } from "./fs-utils";
+import { Setter } from "solid-js";
 
-type ImageOpen = 'base' | 'watermark';
+type ImageOpen = "base" | "watermark";
 
 export async function openImage(
-    setLoc: Setter<string>,
-    setImage: Setter<string>,
-    setFolderSrc: Setter<string>,
-    typeImage: ImageOpen,
+  setLoc: Setter<string>,
+  setImage: Setter<string>,
+  setFolderSrc: Setter<string>,
+  typeImage: ImageOpen,
 ) {
-    let imageLoc;
-    if (typeImage == 'base') {
-        imageLoc = await getImages();
-    } else {
-        imageLoc = await getWatermarkImage();
-    }
-    if (!imageLoc) return;
+  let imageLoc;
+  if (typeImage == "base") {
+    imageLoc = await getImages();
+  } else {
+    imageLoc = await getWatermarkImage();
+  }
+  if (!imageLoc) return;
 
-    let strPath = imageLoc.path;
-    if (!strPath) return;
-    if (typeImage == 'base') {
-        let parent = strPath.split('/');
-        parent.pop();
+  let strPath = imageLoc.path;
+  if (!strPath) return;
+  if (typeImage == "base") {
+    let parent = strPath.split("/");
+    parent.pop();
 
-        const par = parent.join('/');
+    const par = parent.join("/");
 
-        setFolderSrc(par);
-    }
-    setLoc(imageLoc.path);
-    const image = convertFileSrc(imageLoc.path);
-    setImage(image);
+    setFolderSrc(par);
+  }
+  setLoc(imageLoc.path);
+  console.log("path ", imageLoc.path);
+  const image = convertFileSrc(imageLoc.path);
+  console.log({ image });
+  setImage(image);
 }
