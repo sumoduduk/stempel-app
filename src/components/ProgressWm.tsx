7301c61c07dba1e87ce11d0ff9924606c48e7c5d
sumoduduk <sumoduduk13@gmail.com>
2024-03-02 11:29:17 +0700

    feat(ts) : add spinner inside progress

--- src/components/ProgressWm.tsx
+++ src/components/ProgressWm.tsx
@@ -1,6 +1,7 @@
 import { Progress, ProgressLabel, ProgressValueLabel } from "./ui/progress";
 import stateProgress from "../state/progress";
 import { createMemo } from "solid-js";
+import Spinner from "./spinner";
 
 export function ProgressWm() {
   const { progress } = stateProgress;
@@ -13,9 +14,12 @@ export function ProgressWm() {
       minValue={0}
       maxValue={100}
       getValueLabel={({ value }) => `${value}%`}
+      class="w-full text-white"
     >
-      <div class="flex justify-between">
-        <ProgressLabel>Processing...</ProgressLabel>
+      <div class="my-1 flex w-full justify-between px-4">
+        <ProgressLabel class="w-full">
+          <p>Processing...</p> <Spinner width="4" height="4" />
+        </ProgressLabel>
         <ProgressValueLabel />
       </div>
     </Progress>
