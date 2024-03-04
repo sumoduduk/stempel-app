fe9a86c5885e92e9e1bedf78f00ea170b87b120c
sumoduduk <sumoduduk13@gmail.com>
2024-03-04 07:59:23 +0700

    chores(ts): move the label progress

--- src/components/ProgressWm.tsx
+++ src/components/ProgressWm.tsx
@@ -1,7 +1,6 @@
-import { Progress, ProgressLabel, ProgressValueLabel } from "./ui/progress";
+import { Progress, ProgressValueLabel } from "./ui/progress";
 import stateProgress from "../state/progress";
 import { createMemo } from "solid-js";
-import Spinner from "./spinner";
 
 export function ProgressWm() {
   const { progress } = stateProgress;
@@ -16,12 +15,7 @@ export function ProgressWm() {
       getValueLabel={({ value }) => `${value}%`}
       class="w-full text-white"
     >
-      <div class="my-1 flex w-full justify-between px-4">
-        <ProgressLabel class="w-full">
-          <p>Processing...</p> <Spinner width="4" height="4" />
-        </ProgressLabel>
-        <ProgressValueLabel />
-      </div>
+      <ProgressValueLabel />
     </Progress>
   );
 }
