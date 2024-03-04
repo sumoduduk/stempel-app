ca4c1e76b209202b52c5814b3776ba51e2826ab6
sumoduduk <sumoduduk13@gmail.com>
2024-03-04 09:34:28 +0700

    chore(ts) : refactor hero component

---
+++ src/components/hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div class="flex h-1/2 w-full flex-col items-center justify-center">
+      <h1 class="text-3xl">STEMPEL</h1>
+      <h2 class="text-lg">Fast Image Watermarker</h2>
+    </div>
+  );
+}
