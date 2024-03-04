21c324b6994e88e680da12e2a292da7d24b6f35e
sumoduduk <sumoduduk13@gmail.com>
2024-03-04 11:22:13 +0700

    chore (ts) : resize hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
     <div class="flex h-1/2 w-full flex-col items-center justify-center">
-      <h1 class="text-3xl">STEMPEL</h1>
-      <h2 class="text-lg">Fast Image Watermarker</h2>
+      <h1 class="text-5xl font-bold">STEMPEL</h1>
+      <h2 class="text-lg">Parallel Image Watermarker</h2>
     </div>
   );
 }
