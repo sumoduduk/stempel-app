13ce98a0fd843597eb2770c7f961b602ea4900a6
sumoduduk <sumoduduk13@gmail.com>
2024-03-06 16:12:28 +0700

    feat: make Brand component responsive

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,7 +1,9 @@
 export default function Brand() {
   return (
-    <div class="fixed left-[10%] -z-50 origin-left rotate-90 text-white opacity-20">
-      <h1 class="text-[max(10em,20vw)] font-black">STEMPEL</h1>
+    <div class="fixed left-[10%] -z-50 origin-left rotate-90 select-none text-white opacity-20 lg:left-[5%]">
+      <h1 class="text-[max(10em,20vw)] font-black lg:text-[max(5em,10vw)]">
+        STEMPEL
+      </h1>
     </div>
   );
 }
