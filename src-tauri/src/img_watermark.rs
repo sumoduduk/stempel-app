80d1bef8cb8d16b90e46146396f9cd42638c3edb
sumoduduk <sumoduduk13@gmail.com>
2024-03-06 14:44:03 +0700

    feat(rust) : add variable to record elapsed time

--- src-tauri/src/img_watermark.rs
+++ src-tauri/src/img_watermark.rs
@@ -11,6 +11,8 @@ use std::sync::mpsc::channel;
 use std::thread;
 use tauri::{Manager, Runtime};
 
+use std::time::Instant;
+
 use self::load_base::base_load_file;
 use self::load_watermark::wm_load_file;
 use self::multi_thread::multi_thread_opr;
@@ -25,7 +27,7 @@ pub async fn watermark_command<R: Runtime>(
     global_scale: f32,
     wm_scale: f32,
     window: tauri::Window<R>,
-) -> Result<(), String> {
+) -> Result<u128, String> {
     let path_src = Path::new(&path_src);
     let water_path = Path::new(&water_path);
 
@@ -34,6 +36,8 @@ pub async fn watermark_command<R: Runtime>(
     }
 
     let water_ext = water_path.extension().and_then(std::ffi::OsStr::to_str);
+
+    let start = Instant::now();
 
     let water_buff = match water_ext {
         Some("jpg") | Some("jpeg") | Some("png") | Some("webp") => {
@@ -100,5 +104,7 @@ pub async fn watermark_command<R: Runtime>(
         multi_thread_opr(path_src, images, water_buff, coordinate, tx);
     }
 
-    Ok(())
+    let duration = start.elapsed();
+    let milis = duration.as_millis();
+    Ok(milis)
 }
