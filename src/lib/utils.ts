331398de75e1d3c4cdbd3f175ee6737449feb5e5
sumoduduk <sumoduduk13@gmail.com>
2024-03-06 14:45:13 +0700

    chore : return the invoke function from rust side

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,21 +1,21 @@
-import { invoke } from '@tauri-apps/api/core';
-import type { ClassValue } from 'clsx';
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-import { InvokeParamsType } from '~/types/img-types';
+import { invoke } from "@tauri-apps/api/core";
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { InvokeParamsType } from "~/types/img-types";
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function convInt(num: number) {
-    return Math.floor(num);
+  return Math.floor(num);
 }
 
 export async function startInvoke(param: InvokeParamsType) {
-    try {
-        await invoke('watermark_command', param);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    return await invoke("watermark_command", param);
+  } catch (error) {
+    console.error(error);
+  }
 }
