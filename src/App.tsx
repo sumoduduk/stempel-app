b071c9de102c58a6015b75b67c029cba8f3db63b
sumoduduk <sumoduduk13@gmail.com>
2024-03-06 18:26:30 +0700

    chore(ui) : change text color of proceed button

--- src/App.tsx
+++ src/App.tsx
@@ -269,7 +269,7 @@ function App() {
               <button
                 onClick={sendData}
                 disabled={!canProceed()}
-                class="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-100 [box-shadow:5px_5px_rgb(82_82_82)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)]"
+                class="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-200 transition-all duration-100 [box-shadow:5px_5px_rgb(82_82_82)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)]"
               >
                 PROCEED
               </button>
