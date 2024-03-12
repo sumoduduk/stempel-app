09c049c329090d72038ed4983b4d9ea71fcd1235
sumoduduk <sumoduduk13@gmail.com>
2024-03-12 16:35:23 +0700

    feat: add content on about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,7 +1,14 @@
 const About = () => {
   return (
-    <div class="m-auto w-full">
-      <h1 class="text-center text-4xl font-black">About Us</h1>
+    <div class="m-auto flex h-full w-full">
+      <div class="m-auto">
+        <div class="flex flex-col items-center justify-center space-y-1">
+          <h1 class="text-center font-nueu text-4xl font-bold">About Us</h1>
+          <img src="logo.png" />
+          <h2 class="font-bold">Stempel Inc.</h2>
+          <h3>Indonesian Made Software</h3>
+        </div>
+      </div>
     </div>
   );
 };
