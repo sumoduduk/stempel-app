81f3c9b4e2c34471d48cc732ccb82b971ca68a12
sumoduduk <sumoduduk13@gmail.com>
2024-03-13 01:34:11 +0700

    chore (ui) : change inner text html to lang field json

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,12 +1,17 @@
+import lang from "~/assets/lang.json";
+
 const About = () => {
+  const { about } = lang;
   return (
     <div class="m-auto flex h-full w-full">
       <div class="m-auto">
         <div class="flex flex-col items-center justify-center space-y-1">
-          <h1 class="text-center font-nueu text-4xl font-bold">About Us</h1>
+          <h1 class="text-center font-nueu text-4xl font-bold">
+            {about.title.eng}
+          </h1>
           <img src="logo.png" />
           <h2 class="font-bold">Stempel Inc.</h2>
-          <h3>Indonesian Made Software</h3>
+          <h3>{about.h3.eng}</h3>
         </div>
       </div>
     </div>
